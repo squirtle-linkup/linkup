7e62d38bf4311db54f7b01405279db36ccb84101
Samuel Lee <leesamuel423@gmail.com>
2023-05-24 04:30:30 -0400

    initialize AllLinks component of Dashboard
    Co-authored-by: Jackie Yuan <yuanjackie1@users.noreply.github.com>
    Co-authored-by: Timmy Zhu <timzhu15@users.noreply.github.com>
    Co-authored-by: Dylan Hawkins<hawkinsd@users.noreply.github.com>

---
+++ src/client/components/Dashboard/AllLinks.tsx
@@ -0,0 +1,34 @@
+import { useNavigate } from 'react-router-dom';
+import styles from '../../stylesheets/Dashboard/AllLinks.module.scss';
+
+const AllLinks = () => {
+  const navigate = useNavigate();
+  const assets = ['bear.png', 'meerkat.png', 'rabbit.png', 'sloth.png', 'snake.png'];
+
+  
+
+  const getRandomImage = () => {
+    const randomIndex = Math.floor(Math.random() * assets.length);
+    return `/src/client/assets/${assets[randomIndex]}`;
+  }
+  
+  return (
+    <div className={styles.allLinks}>
+      <div className={styles.header}>
+        <h1>All Links</h1>
+        <button className={ styles.addLink} onClick={() => navigate('/newLink')}>+ New Link</button>
+      </div>
+      <div className={styles.connection}>
+
+    </div>
+      <button onClick={() => navigate('/links')}><p>name</p><img src="/src/client/assets/biden.jpg"/> </button>
+      <button onClick={() => navigate('/links')}><p>name</p><img src={getRandomImage()}/> </button>
+      <button onClick={() => navigate('/links')}><p>name</p><img src={getRandomImage()}/> </button>
+      <button onClick={() => navigate('/links')}><p>name</p><img src={getRandomImage()}/> </button>
+      <button onClick={() => navigate('/links')}><p>name</p><img src={getRandomImage()}/> </button>
+      <button onClick={() => navigate('/links')}><p>name</p><img src={getRandomImage()}/> </button>
+    </div>
+  )
+}
+
+export default AllLinks;
