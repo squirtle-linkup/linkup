0c9d0df48c6f2b9cf082c50f7c9a4d718ba2c98f
Samuel Lee <leesamuel423@gmail.com>
2023-05-24 04:36:57 -0400

    remove view buttons on Dashboard

--- src/client/components/Dashboard/Dashboard.tsx
+++ src/client/components/Dashboard/Dashboard.tsx
@@ -2,22 +2,10 @@ import { useNavigate } from 'react-router-dom';
 import AllLinks from './AllLinks';
 
 const Dashboard = () => {
-  const navigate = useNavigate();
 
-  const handleAddLink = () => {
-    navigate('/newLink');
-  }
-  const handleEditLink = () => {
-    navigate('/linksEdit');
-  }
-  const handleViewLink = () => {
-    navigate('/links');
-  }
   return (
     <div>
       <h1>Dashboard</h1>
-      <button onClick={handleEditLink}>Edit Link</button>
-      <button onClick={handleViewLink}>View Link</button>
       <AllLinks />
     </div>
   )
